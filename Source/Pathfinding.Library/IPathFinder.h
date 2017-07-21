272c58bd33462cc0b6c5ef1f7b06c2c537b8bb7d
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-07-20 23:48:07 -0400

    Removed a overloaded FindPath()

--- Source/Pathfinding.Library/IPathFinder.h
+++ Source/Pathfinding.Library/IPathFinder.h
@@ -2,18 +2,13 @@
 
 #include "Node.h"
 #include <deque>
-#include <set>
-#include <memory>
 
 namespace Library
-{
-	class Graph;
-
+{	
 	class IPathFinder
 	{
 	public:
 		virtual ~IPathFinder() { };
-		virtual std::deque<std::shared_ptr<Node>> FindPath(std::shared_ptr<Node> start, std::shared_ptr<Node> end, std::set<std::shared_ptr<Node>>& closedSet) = 0;
-		std::deque<std::shared_ptr<Node>> FindPath(std::shared_ptr<Node> start, std::shared_ptr<Node> end);
+		virtual std::deque<std::shared_ptr<Node>> FindPath(std::shared_ptr<Node> start, std::shared_ptr<Node> end, std::uint32_t& numberOfNodesVisited) = 0;			
 	};
 }
